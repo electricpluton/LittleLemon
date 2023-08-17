b8c951cc13f37a2116621cc542321e44fe5491a7
kezou <kezou@madisoncollege.edu>
2023-08-17 14:05:11 -0500

    Create Little lemon project and restaurant app

---
+++ restaurant/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path('items/', views.MenuItemsView.as_view()),
+    path('items/<int:pk>', views.SingleMenuItemView.as_view())
+]
---
+++ restaurant/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import generics , viewsets
+from .serializers import MenuSerializer, BookingSerializer
+from . models import Menu, Booking
+
+# Create your views here.
+
+class MenuItemsView(generics.ListCreateAPIView):
+    
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
